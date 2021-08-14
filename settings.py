bd95e0f5b352346cdb5d0e7b4c2196fc48a7a4f6
Matt Ashorn <matthew.ashorn@utexas.edu>
2021-08-14 07:58:04 -0500

    Changed alien logo, and changed bullet color to white to contrast black background.

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (200, 200, 200)
         self.bullets_allowed = 8
         
         
